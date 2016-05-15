8fa7ae111288938ce8130b05e17c47030924a58e
jobscheffers_cp <SND\jobscheffers_cp>
2016-05-15 20:47:25 +0000

    fixed error in versioning. added stop method in HostTableManager

--- BridgeFundamentals/BridgeNetworkProtocol2/Properties/AssemblyInfo.cs
+++ BridgeFundamentals/BridgeNetworkProtocol2/Properties/AssemblyInfo.cs
@@ -7,6 +7,9 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("BridgeNetworkProtocol")]
 [assembly: AssemblyProduct("BridgeNetworkProtocol")]
+[assembly: AssemblyDescription("Base classes for implementing the network protocol required to participate in computerbridge matches (http://www.computerbridge.com)")]
+[assembly: AssemblyCompany("Sodes")]
+[assembly: AssemblyCopyright("Copyright © Sodes 2016")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -26,7 +29,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 // to avoid FxCop errors:
