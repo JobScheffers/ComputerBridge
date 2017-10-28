c366d492cdc817bb776a8c9f3a96df609c17d263
Job Scheffers <job.scheffers@eur.corp.vattenfall.com>
2017-10-28 23:12:08 +0200

    prevent duplicate unittests

---
+++ BridgeFundamentals/BridgeFundamentals.UnitTests/CacheTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sodes.Base;
+
+namespace Sodes.Bridge.Base.Test
+{
+	[TestClass]
+	public class CacheTest
+	{
+        [TestMethod, TestCategory("CI"), TestCategory("Bid")]
+        public void Cache_Test1()
+        {
+            TestCache.Init();
+            var x = "Hello";
+            TestCache.Instance.Add("test", x);
+            var y = (string)TestCache.Instance.Get("test");
+            Assert.AreEqual(x, y);
+        }
+	}
+
+    public class TestCache : Cache
+    {
+
+        public override void Add(string key, object x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object Get(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Init()
+        {
+            TestCache.Instance = new TestCache();
+        }
+    }
+}
