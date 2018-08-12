ca9dd311a3ba128a87c158c62130942db9d92a7a
Job Scheffers <job.scheffers@eur.corp.vattenfall.com>
2018-08-12 22:31:32 +0200

    experimenting with unit tesp project format

---
+++ BridgeFundamentals.Standard/ClassLibrary1/TableManagerClientTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace Bridge.Networking.UnitTests
+{
+    [TestClass]
+    public class TableManagerClientTests
+    {
+        [TestMethod]
+        public async Task ABrandNewTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
